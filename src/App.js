94f89390696ba24d137544c5ea73e0a0e6391834
boban.alempijevic <boban.alempijevic@gmail.com>
2023-06-14 14:17:56 +0300

    First update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last name: Doe</h2>
+      <h2>Age: 30</h2>
+    </>
+  );
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {2 +2}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name={'Boban'}/>
     </div>
   );
 }
