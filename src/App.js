3123da516c0db438aab6e5913bdfde3486364672
boban.alempijevic <boban.alempijevic@gmail.com>
2023-06-14 14:42:23 +0300

    second update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import './App.css';
 
+/**
+ * 
+ * @param {*} props 
+ * @returns Component
+ */
 const Person = (props) => {
   return (
     <>
       <h1>Name: {props.name}</h1>
-      <h2>Last name: Doe</h2>
-      <h2>Age: 30</h2>
+      <h2>Last name: {props.lastName}</h2>
+      <h2>Age: {props.age}</h2>
     </>
   );
 }
 
+/**
+ * 
+ * @returns App DIV
+ */
 const App = () => {
   return (
     <div className="App">
-      <Person name={'Boban'}/>
+      <Person 
+        name={'Boban'} 
+        lastName={'Alempijevic'} 
+        age={44}
+      />
+      <Person
+        name={'Petra'}
+        lastName={'Hultin'}
+        age={30}
+      />
     </div>
   );
 }
